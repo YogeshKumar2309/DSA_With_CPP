950aac170add2acfdb2762a4fdab8f073d47327f
YogeshKumar2309 <placementsbyyogesh@gmail.com>
2025-10-22 21:29:12 +0530

    compted function practice and homework

--- 3.Funtion/Hw.cpp
+++ 3.Funtion/Hw.cpp
@@ -1,5 +1,88 @@
 //!HomeWork
+
+#include <iostream>
+using namespace std;
+
 //? WAF to check if a number is prime or not.
+int checkPrime(int n){
+  if(n <= 1) return 0;  
+  for(int i = 2; i < n; i++){
+    if(n%i == 0){
+      return 0;
+    }    
+  }  
+  return 1;
+}
+
 //? WAF to print all prime numbers from 2 to N.
+void AllPrimeNo(int n){
+  int count = 2;
+  int primeCount = 0;
+while(primeCount < n){
+  if(checkPrime(count)){
+    cout << count << " ";
+    primeCount++;
+  }
+  count++;
+}
+}
+
 //? WAF to print nth Fibonacci.
+void nthFibonacci(int n){
+  int first = 0;
+  int second = 1;
+  for(int i = 0; i < n; i++){
+    cout << first << " ";
+    int next = first + second;
+    first = second;
+    second = next;
+  }
+}
+
 //? learn switch and one eg.
+void printSundayMonday(int n){
+  switch (n)
+  {
+  case 1:
+    cout << "Monday";
+    break;
+  case 2:
+    cout << "Tuesday";
+    break;
+  case 3:
+    cout << "Wednesday";
+    break;
+  case 4:
+    cout << "Thursday";
+    break;
+  case 5:
+    cout << "Friday";
+    break;
+  case 6:
+    cout << "Suturday";
+    break;
+  case 7:
+    cout << "Sunday";
+    break;
+
+  default:
+    cout << "not match!";
+    break;
+  }
+}
+
+int main(){
+  // bool prime = checkPrime(9);
+  // if(prime){
+  //   cout << "prime";
+  // } else {
+  //   cout << "NotPrime";
+  // }
+
+  // AllPrimeNo(10);
+
+  // nthFibonacci(10);
+
+  printSundayMonday(5);
+  return 0;
+}
--- 3.Funtion/basic.cpp
+++ 3.Funtion/basic.cpp
@@ -1,4 +1,56 @@
+#include <iostream>
+using namespace std;
+
 //? Calculate sum of numbers from 1 to N.
-//? Calculate N Factorial
+void CalSum(int n) {
+  int sum = 0;
+  for(int i = 0 ; i <= n ; i++){
+      sum += i;
+    }
+    cout << sum;
+}
+ //? Calculate N Factorial
+void CalNFact(int n) { 
+    double fact = 1;
+    for(int i = 1; i <= n; i++){
+      fact *= i;
+    }
+    cout << fact;
+}
+
 //? Calculate sum of digits of a number.
+void SumOfDigits(int digit) {
+  int sumOfDigit = 0;
+  while(digit > 0){
+    int lastDigit = digit%10;
+    sumOfDigit += lastDigit;
+    digit /= 10;
+  }
+  cout << sumOfDigit;
+}
+
 //? Calculate nCr binomial coefficent for n & r
+int fact(int n){
+  double fact = 1;
+  for(int i = 1; i <= n; i++){
+    fact *= i;
+  }
+  return fact;
+}
+
+void nCr (int n, int r) {
+  int factN = fact(n);
+  int factR = fact(r);
+  int factNR = fact(n -r);
+  int NCR = factN/(factR * factNR);
+  cout << NCR;  
+}
+
+int main () {
+    // CalSum(10);
+    // CalNFact(4);
+    // SumOfDigits(2319);
+    nCr(5,2);   
+   
+  return 0;
+}

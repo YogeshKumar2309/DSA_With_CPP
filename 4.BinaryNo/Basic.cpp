8922a32976166a95b79038d30389e1e4695b8d8f
YogeshKumar2309 <placementsbyyogesh@gmail.com>
2025-10-23 20:18:00 +0530

    practive , bitwise opr and binary , only 5% learn

--- 4.BinaryNo/Basic.cpp
+++ 4.BinaryNo/Basic.cpp
@@ -1,3 +1,80 @@
+#include <iostream>
+using namespace std;
+
 //? Decimal to Binary
+void DecimalToBinary(int decimal)
+{
+  int binary[32];
+  int index = 0;
+
+  if (decimal == 0)
+  {
+    cout << "0";
+    return;
+  }
+
+  while(decimal > 0){
+    binary[index] = decimal % 2;
+    decimal /= 2;
+    index++;
+  }
+
+  //print the reverse order
+  for(int i = index -1; i >= 0; i--){
+    cout << binary[i];
+  }
+}
+
 //? Binary to Decimal
+void BinarytoDecimal(long long binary){
+  int decimal = 0;
+  int base = 1;
+
+  while(binary > 0){
+    int lastDigit = binary % 10;
+    decimal += (lastDigit * base);
+    base *= 2;
+    binary /= 10;
+  }
+
+  cout << "Decimal: " << decimal;
+}
+
 //? Convert -8 to binary & reverse.
+void DecimalToNegativeBinary(int num){
+    if (num >= 0) {
+        cout << "Please enter a negative number only!";
+        return;
+    }
+
+    int bits = 8;
+    int posValue = (1 << bits) + num; // 2^bits + num  → Two's complement
+
+    int binary[32];
+    int index = 0;
+
+    while(posValue > 0){
+      binary[index++] = posValue % 2;
+      posValue /= 2;
+    }
+
+    //if bits not completed then fill 0
+    while( index < bits) {
+      binary[index++] = 0;
+    }
+
+    cout << "Binary (" << bits << "-bit): ";
+    for(int i = bits - 1; i >= 0; i--){
+      cout << binary[i];
+    } 
+  }
+
+
+int main()
+{
+  // DecimalToBinary(105);
+  // BinarytoDecimal(101);
+  DecimalToNegativeBinary(-5);
+
+  return 0;
+}
--- 5.BitwiseOpr/Hw.cpp
+++ 5.BitwiseOpr/Hw.cpp
@@ -1,13 +1,41 @@
 //? Sove for
 /**
- * 6 & 10
- * 1 | 10
- * 6 ^ 10
- * 
- * 10 << 2
- * 10 >> 1
+ * 6 & 10 = 2
+ * 1 | 10 = 11
+ * 6 ^ 10 = 12
+ *
+ * 10 << 2 = 40
+ * 10 >> 1 = 5
  */
 
- //? Figure out how to find if a number is power of 2 wihtout any loop.
- //? WAF to reverse an Integer n.
- 
+#include <iostream>
+using namespace std;
+
+//? Figure out how to find if a number is power of 2 wihtout any loop.
+bool isPowerOf2(int n){
+  return (n > 0) && ((n & (n - 1)) == 0);
+}
+
+//? WAF to reverse an Integer n.
+int reverseInteger(int n){
+  int rev = 0;
+  while(n != 0){
+    int digit = n % 10;
+    rev = rev * 10 + digit;
+    n /= 10;
+  }
+  return rev;
+}
+
+int main()
+{
+  int n = 8;
+
+  if (isPowerOf2(n))
+    cout << n << " is a power of 2" << endl;
+  else
+    cout << n << " is NOT a power of 2" << endl;
+
+  // cout << reverseInteger(54321);
+  return 0;
+}
